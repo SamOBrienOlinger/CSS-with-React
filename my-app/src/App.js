c35a2480af4f10bd1e2f8233ea0b26e1d10a6ab7
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-02-27 13:49:44 +0000

    add  StatefulGreetingWithPrevState.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWIthPrevState';
 
 function App() {
   return (
     <div>
       {/* Add your components here */}
-      <Sidebar />
+      {/* <Sidebar /> */}
+      {/* <StatefulGreeting /> */}
+      {/* <StatefulGreetingWithCallback /> */}
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
