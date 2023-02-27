ebde11caea80603a01a64832eb40162f65e03afa
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-02-27 15:18:58 +0000

    ES7 extension added with top snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function ES7Snippets() {
+  return (
+    <div>ES7Snippets</div>
+
+
+  )
+}
+
+import React from 'react'
+
+export const ES7Snippets = () => {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+import React, { Component } from 'react'
+
+export class ES7Snippets extends Component {
+  render() {
+    return (
+      <div>ES7Snippets</div>
+    )
+  }
+}
+
+export default ES7Snippets
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+console.log(first)
+
+
+console.log('first', first)
+
